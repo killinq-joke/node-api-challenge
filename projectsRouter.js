92be212a4a6c153b985673cdee85a3964859c08c
Zak <zakadmaster@gmail.com>
2020-03-13 10:15:24 +0100

    Addition of all CRUD for project

--- projectsRouter.js
+++ projectsRouter.js
@@ -14,4 +14,65 @@ projects.get("/", (req, res) => {
     });
 });
 
+projects.get("/:id", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .get(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.get("/:id/actions", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .getProjectActions(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.post("/", (req, res) => {
+    const project = req.body;
+  projectsHelpers
+    .insert(project)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const change = req.body;
+  projectsHelpers
+    .update(id, change)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
+projects.delete("/:id", (req, res) => {
+    const {id} = req.params;
+  projectsHelpers
+    .remove(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500);
+    });
+});
+
 module.exports = projects;
